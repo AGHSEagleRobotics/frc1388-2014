3e32685ef92289719f8b3260eea91155d015009a
Joey Kraut <notjoeykrautokaymaybeitis@gmail.com>
2014-03-01 11:44:36 -0800

    Changed autonomous things as per the code review

--- Robot2014/Commands/AutonomousCommand.cpp
+++ Robot2014/Commands/AutonomousCommand.cpp
@@ -18,8 +18,6 @@ AutonomousCommand::AutonomousCommand() {
 }
 // Called just before this Command runs the first time
 void AutonomousCommand::Initialize() {
-//	TODO: set the default autonomous to AutonomousGroup() not AutonomousTest()
-//	(new AutonomousTest())->Start();
 	(new AutonomousGroup())->Start();
 }
 // Called repeatedly when this Command is scheduled to run
@@ -28,7 +26,7 @@ void AutonomousCommand::Execute() {
 }
 // Make this return true when this Command no longer needs to run execute()
 bool AutonomousCommand::IsFinished() {
-	return false;
+	return true;
 }
 // Called once after isFinished returns true
 void AutonomousCommand::End() {
